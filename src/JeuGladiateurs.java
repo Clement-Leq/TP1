df37382c9c81f0e4add02c546b39aa2363250652
Clement <clementleq@gmail.com>
2019-09-17 14:21:01 -0400

    Fichier de base

---
+++ src/JeuGladiateurs.java
@@ -0,0 +1,18 @@
+
+import personnage.Personnage;
+import combat.CompteurDeTour;
+import combat.AffichageEcran;
+
+public class JeuGladiateurs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CompteurDeTour tour = new CompteurDeTour();
+        AffichageEcran affichage = new AffichageEcran();
+        Personnage personnage1 = new Personnage(); //L'initialisation du personnage doit se faire dans le constructeur
+        Personnage personnage2 = new Personnage(); //L'initialisation du personnage doit se faire dans le constructeur
+    }
+
+}
