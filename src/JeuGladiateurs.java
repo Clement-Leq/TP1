8a2914833876a56c0bbd23a69354ae0cba1a924d
Clement <clementleq@gmail.com>
2019-09-18 15:09:57 -0400

    Écran début combat + boucle initiative

--- src/JeuGladiateurs.java
+++ src/JeuGladiateurs.java
@@ -11,8 +11,25 @@ public class JeuGladiateurs {
     public static void main(String[] args) {
         CompteurDeTour tour = new CompteurDeTour();
         AffichageEcran affichage = new AffichageEcran();
-        Personnage personnage1 = new Personnage(); //L'initialisation du personnage doit se faire dans le constructeur
-        Personnage personnage2 = new Personnage(); //L'initialisation du personnage doit se faire dans le constructeur
+        Personnage personnage1 = new Personnage("Bob le malchanceux", 15, 15, 70, 15); //L'initialisation du personnage doit se faire dans le constructeur
+        Personnage personnage2 = new Personnage("Igor l'empaleur", 25, 5, 100, 30); //L'initialisation du personnage doit se faire dans le constructeur
+        
+        personnage1.afficherInfosPersonnage();
+        personnage2.afficherInfosPersonnage();
+        System.out.println("<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>\r\n" + 
+        		">>>>> Que le combat commence ! <<<<<\r\n" + 
+        		"<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>");
+        
+        while(!personnage1.isEstMort() && !personnage2.isEstMort()) {
+        	
+        	for(int i = 0; i < 100; i++) {
+        		
+        	}
+        	System.out.println("   Statut : Vivant\n");
+        	
+        	System.out.println("   Statut : Mort\n");
+        	personnage1.setEstMort(true);
+        }
     }
 
 }
--- src/personnage/Personnage.java
+++ src/personnage/Personnage.java
@@ -2,29 +2,106 @@
 package personnage;
 
 public class Personnage {
-
+	private String nom;
+	private int attMax;
+	private int def;
+	private int pv;
+	private int init;
+	boolean estMort = false;
+	
     // -------------------------------------------------------------------------
-    public Personnage(String nom, int attaqueMax, int defense, int pvs, int ini) {
+	
+    public Personnage(String nom, int attMax, int def, int pv, int init) {
         // TODO : Constructeur AVEC paramètres
+    	this.nom = nom;
+    	this.attMax = attMax;
+    	this.def = def;
+    	this.pv = pv;
+    	this.init = init;
     }
 
     // -------------------------------------------------------------------------
+    
     public Personnage() {
         // TODO : Constructeur SANS paramètres qui initialise à ZÉRO ou à ""
+    	nom = "";
+    	attMax = 0;
+    	def = 0;
+    	pv = 0;
+    	init = 0;
+    	estMort = false;
     }
 
     // -------------------------------------------------------------------------
     // TODO : Les getters
+    
+    public String getNom() {
+		return nom;
+	}
+    
+    public int getAttMax() {
+		return attMax;
+	}
+    
+    public int getDef() {
+		return def;
+	}
+    
+    public int getPv() {
+		return pv;
+	}
+    
+    public int getInit() {
+		return init;
+	}
+    
+    public boolean isEstMort() {
+		return estMort;
+	}
 
     // -------------------------------------------------------------------------
     // TODO : Les setters
+    
+    public void setNom(String nom) {
+		this.nom = nom;
+	}
+    
+    public void setAttMax(int attMax) {
+		this.attMax = attMax;
+	}
+    
+    public void setDef(int def) {
+		this.def = def;
+	}
+    
+    public void setPv(int pv) {
+		this.pv = pv;
+	}
+    
+    public void setInit(int init) {
+		this.init = init;
+	}
+    
+    public void setEstMort(boolean estMort) {
+		this.estMort = estMort;
+	}
 
     // -------------------------------------------------------------------------
+    
     public void afficherInfosPersonnage() {
         // TODO : Afficher les infos du personnage, tel que montré dans l'énoncé
+    	System.out.println(nom);
+    	System.out.println("   Attaque : " + attMax);
+    	System.out.println("   D�fense : " + def);
+    	System.out.println("   Points de vie : " + pv);
+    	System.out.println("   Initiative : " + init);
+    	
+    	if(estMort == true) System.out.println("   Statut : Mort\n ");
+    	else System.out.println("   Statut : Vivant\n");
     }
 
-    // -------------------------------------------------------------------------
+	// -------------------------------------------------------------------------
+    
     private int attaqueCalcul() {
         // TODO : Retourner la valeur de l'attaque du personnage.
         // Cette valeur est trouvée aléatoirement et doit se situer entre ZÉRO et valeurMaxAttaque.
@@ -32,6 +109,7 @@ public class Personnage {
     }
 
     // -------------------------------------------------------------------------
+    
     public void frapperPersonnage(Personnage personnageCible) {
         // TODO : Récupérer la valeur d'attaque pour ce tour, calculer les dégats,
         //modifier les points de vie du personnage cible, afficher les détails
@@ -39,6 +117,7 @@ public class Personnage {
     }
 
     // -------------------------------------------------------------------------
+    
     public void setNewIniRandom() {
         // TODO : Modifier de façon aléatoire la valeur INI du personnage.
     }
